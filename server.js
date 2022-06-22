34d9ba553cf219da7768b74cb22e8faada780d8b
Ryan Acham <ryanacham@hotmail.com>
2022-06-21 20:48:24 -0400

    did last add function and update function

--- server.js
+++ server.js
@@ -146,6 +146,8 @@ const addEmp = async () => {
         });
 
         console.log(`${answer.firstName} ${answer.lastName} added successfully.\n`);
+        userQuery();
+
 }
 
 const addDep = async () => {
@@ -164,13 +166,91 @@ const addDep = async () => {
     });
 
     console.log(`${answer.deptName} added successfully to departments.\n`)
-    initialAction();
+    userQuery();
 }
 
 const addRole = async () => {
+    console.log('Role Add');
 
-}
+        let departments = await conn.query("SELECT * FROM department")
+
+        let answer = await inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'Role Name?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'Role Salary?'
+            },
+            {
+                name: 'departmentId',
+                type: 'list',
+                choices: departments.map((departmentId) => {
+                    return {
+                        name: departmentId.department_name,
+                        value: departmentId.id
+                    }
+                }),
+                message: 'Department ID?',
+            }
+        ]);
+        
+        let chosenDepartment;
+        for (i = 0; i < departments.length; i++) {
+            if(departments[i].dept_id === answer.choice) {
+                chosenDepartment = departments[i];
+            };
+        }
+        let result = await conn.query("INSERT INTO role SET ?", {
+            title: answer.title,
+            salary: answer.salary,
+            dept_id: answer.departmentId
+        })
+
+        console.log(`${answer.title} role added successfully.\n`)
+        userQuery();
+    }
 
 const upRole = async () => {
+        console.log('EMPLOYEE UPDATE');
+        
+        let employees = await conn.query("SELECT * FROM employee");
 
-}
+        let employeeSelection = await inquirer.prompt([
+            {
+                name: 'employee',
+                type: 'list',
+                choices: employees.map((employeeName) => {
+                    return {
+                        name: employeeName.first_name + " " + employeeName.last_name,
+                        value: employeeName.id
+                    }
+                }),
+                message: 'Which employee are you trying to update?'
+            }
+        ]);
+
+        let roles = await conn.query("SELECT * FROM role");
+
+        let roleSelection = await inquirer.prompt([
+            {
+                name: 'role',
+                type: 'list',
+                choices: roles.map((roleName) => {
+                    return {
+                        name: roleName.title,
+                        value: roleName.id
+                    }
+                }),
+                message: 'Employee Role?'
+            }
+        ]);
+
+        let result = await conn.query("UPDATE employee SET ? WHERE ?", [{ role_id: roleSelection.role }, { id: employeeSelection.employee }]);
+
+        console.log(`The role was successfully updated.\n`);
+        userQuery();
+    }
